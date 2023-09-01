25119f1eb32a25024e6d162a2a56fce9d07847a6
Marko Urbanč <urbancmarko1@gmail.com>
2023-09-01 16:58:19 +0200

    style(plots): :art: MPI scaling plots and data sets

--- collect-data.py
+++ collect-data.py
@@ -2,15 +2,33 @@ from gatherstat import GatherStatistics
 import matplotlib.pyplot as plt
 import numpy as np
 
-gatherer = GatherStatistics(rank_range=(1, 3), sample_size=3, step=1)
+# gatherer = GatherStatistics(rank_range=(1, 13), sample_size=10, step=1)
 
-cr, t = gatherer.run()
+# cr, t = gatherer.run()
 
-np.savez("data.npz", cr, t)
+# np.savez("data_full-range_manual.npz", cr, t)
 
-plt.plot(cr, t)
+with np.load("data_full-range.npz") as data:
+    cr = data["arr_0"]
+    t = data["arr_1"]
+
+plt.rcParams.update({'font.family': 'Verdana'})
+fig, ax = plt.subplots(facecolor="#4d4c4c")
+plt.plot(cr, t, c="#a8325e")
 plt.xlabel("Number of ranks")
 plt.ylabel("Time to solve PDE")
-plt.title("Time to solve PDE for different number of ranks")
+plt.suptitle("ColocationSolver.solve_Properly() MPI Scaling", color="#dedede")
+ax.set_facecolor("#bababa")
+plt.grid(c="#d1d1d1", alpha=0.5)
+ax.spines['bottom'].set_color("#dedede")
+ax.spines['top'].set_color("#dedede")
+ax.spines['right'].set_color("#dedede")
+ax.spines['left'].set_color("#dedede")
+ax.xaxis.label.set_color("#dedede")
+ax.yaxis.label.set_color("#dedede")
+ax.tick_params(axis="x", colors="#dedede")
+ax.tick_params(axis="y", colors="#dedede")
+#ax.axhline(0, linestyle="--", color="#dedede")
+#plt.subplots_adjust(right=1)
 plt.show()
 
--- gatherstat.py
+++ gatherstat.py
@@ -90,21 +90,21 @@ from mpi import MPI_Node
 from colocation import ColocationSolver
 from spectral import SpectralSolver
                     
-def initial_condition(x):
-    return np.sin(2*np.pi*x*10)
+def gaussian_Initial(x, a=5, sigma=1):
+    return np.exp(-((-x+a)/2)**2 / sigma**2)
 # Solve for these points
-t = np.linspace(0, 1, 1000)
+t = np.linspace(0, 0.15, 1000)
 # Solve for this grid range
-x_range = (0, 1)
+x_range = (-0.5, 10.5)
 # Solve for this grid size
 N = 1000
 # Solve for this diffusion constant
 D = 1e-5
 # Initialize solver
-solver = ColocationSolver(initial_condition, x_range, N, t, D)
+solver = ColocationSolver(gaussian_Initial, x_range, N, t, D)
 
 node = MPI_Node(solver)
-print(f"@{node.solve()[1]}%")
+print(f"@{node.solve(method='manual')[1]}%")
 """)
 
         code = template.substitute()
